17afd3c195923a0be597df413bb15dc960588a2c
Jonas <jonas.degelo@gmail.com>
2021-01-31 16:43:16 +0100

    Update coordinates to Zurich

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 47.376888;
+  let longitude = 8.541694;
 
   getWeather(latitude, longitude);
 }
